7cc60eaba6e8b642c6a5448505a7e6f82696cd96
Gregg Butensky <gregg@coderefactory.com>
2013-07-10 05:42:53 +0000

    Change to use contents of index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
   var buffer = new Buffer(fs.readFileSync("index.html"));
-  respond.send(buffer.toString());
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
